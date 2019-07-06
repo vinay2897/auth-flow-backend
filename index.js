3ffe5f031ff44b737d53b33b00eebbbc3593c60d
Vinay <vinayshankar2897@gmail.com>
2019-07-06 13:12:44 +0530

    port issue fixed

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const routes = require("./routes")
