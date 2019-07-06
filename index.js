87f41a3ecdd6fa48cd8d1bee131d89f43e4ef250
Vinay <vinayshankar2897@gmail.com>
2019-07-06 13:11:07 +0530

    port issue fixed

--- index.js
+++ index.js
@@ -24,4 +24,10 @@ app.use('/*', function (req, res) {
 });
 /*------------------Middleware---------------*/
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
